ccd6405ca44182a2e5f18c0ddb91bc3e5ec8a2c8
xank <xank@qq.com>
2018-12-25 15:32:28 +0800

    1

---
+++ packages/code-snippet/components/dialog.1.vue
@@ -0,0 +1,59 @@
+<style lang="postcss" scoped>
+</style>
+
+<template>
+  <yd-dialog
+    :class="b()"
+    :title="title"
+    :visible.sync="open"
+    v-loading
+    :close-on-click-modal="true"
+  >
+    <div slot="footer">
+      <el-button
+        type="text"
+        @click="handleClose"
+      >取消</el-button>
+      <el-button
+        @click="handleSubmit"
+        type="primary"
+      >保存</el-button>
+    </div>
+  </yd-dialog>
+</template>
+
+<script>
+import create from 'common/utils/create-basic'
+import { deepClone } from 'common/utils'
+
+export default create({
+  name: 'dialog',
+
+  components: {},
+
+  data() {
+    return {
+      title: '',
+      open: false,
+      form: {
+
+      }
+    }
+  },
+
+  methods: {
+    handleOpen(form) {
+      this.form = deepClone(form)
+      this.open = true
+    },
+
+    handleClose() {
+      this.open = false
+    },
+
+    handleSubmit() {
+      this.$emit('submit', this.form)
+    }
+  }
+})
+</script>
