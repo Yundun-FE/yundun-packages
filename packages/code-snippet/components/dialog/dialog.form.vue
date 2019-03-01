b3cc2f06692580e3d7e98e825835cf03e1bf406a
xank <xank@qq.com>
2019-03-01 18:17:48 +0800

    1

---
+++ packages/code-snippet/components/dialog/dialog.form.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <DialogForm
+      ref="Dialog"
+      v-model="form"
+      :rules="rules"
+      :form-default="formDefault"
+      title=""
+      cancel-button-type="text"
+      @submit="handleSubmit"
+    >
+      <el-form-item label="通知方式">
+        <yd-form-radio/>
+      </el-form-item>
+    </DialogForm>
+  </div>
+</template>
+
+<script>
+import RULE from '@/utils/verify'
+import { validator } from '@/utils/form'
+import createDialog from '@/mixins/createDialog'
+
+export default createDialog({
+  data() {
+    return {
+      rules: {},
+      formDefault: {},
+      form: {}
+    }
+  },
+
+  methods: {
+    handleSubmit() {
+      this.$emit('submit', this.form)
+    }
+  }
+})
+</script>
---
+++ packages/code-snippet/components/template.column.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="b()"></div>
+</template>
+
+<script>
+import create from '@/utils/create-basic'
+import column from '@/mixins/column'
+
+export default create({
+  name: 'Column',
+
+  mixins: [column]
+})
+</script>
