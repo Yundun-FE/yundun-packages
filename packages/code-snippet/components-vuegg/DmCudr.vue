3ccc5256b71d19165ecab04f5da9a244df244c8e
fanbo <xank@qq.com>
2019-06-04 00:29:06 +0800

    1

---
+++ packages/code-snippet/components-vuegg/DmCudr.vue
@@ -0,0 +1,48 @@
+<template>
+  <DmCudr
+    ref="DmCudr"
+    :columns="columns"
+    :loading="loading"
+    :data="list"
+  >
+    <template
+      slot="name"
+      slot-scope="{text, record}"
+    >
+      <!--  -->
+    </template>
+  </DmCudr>
+</template>
+
+<script>
+import consoleData from '@/mixins/consoleData'
+import ColumnName from './components/ColumnName'
+
+export default {
+  components: {
+    ColumnName
+  },
+
+  mixins: [consoleData],
+
+  data() {
+    return {
+      API_INDEX: 'v2/jobs',
+      bindParams: {
+        pageSize: 100
+      },
+      columns: [
+        {
+          title: '名称',
+          dataIndex: 'name',
+          scopedSlots: { customRender: 'name' }
+        },
+      ]
+    }
+  },
+
+  created() {
+    this.fetchList()
+  }
+}
+</script>
