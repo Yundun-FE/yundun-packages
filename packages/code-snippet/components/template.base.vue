d1b6feb9531fe2a12acf63a375bce78ef3926038
fanbo <xank@qq.com>
2019-06-13 00:36:52 +0800

    1

---
+++ packages/code-snippet/components/template.base.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!--  -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+
+  props: {},
+
+  data() {
+    return {}
+  },
+
+  computed: {},
+
+  methods: {}
+}
+</script>
--- packages/code-snippet/store/report.js
+++ packages/code-snippet/store/report.js
@@ -1,17 +1,29 @@
+import Fetch from '@/api/fetch'
+
 const state = {
-  utime: ''
+  id: '',
+  info: {}
 }
 
 const mutations = {
-  SET_UTIME(state) {
-    state.utime = Date.now()
+  SET_ID: (state, id) => {
+    state.id = id
+  },
+
+  SET_INFO: (state, data) => {
+    state.info = data
   }
 }
 
 const actions = {
+  async fetchInfo({ commit, state }) {
+    const data = await Fetch.get('', { id: state.id })
+    commit('SET_INFO', data)
+  }
 }
 
 export default {
+  namespaced: true,
   state,
   mutations,
   actions
